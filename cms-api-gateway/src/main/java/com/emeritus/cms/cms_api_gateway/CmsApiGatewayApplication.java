fc79ec4dcc01cb61aaa7b01ae92677e5438656fc
rajvscode <rajvs1985@gmail.com>
2024-07-26 16:34:54 +0530

    Initial Commit - Basic API Gateway started running

--- cms-api-gateway/src/main/java/com/emeritus/cms/cms_api_gateway/CmsApiGatewayApplication.java
+++ cms-api-gateway/src/main/java/com/emeritus/cms/cms_api_gateway/CmsApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.emeritus.cms.assignment_service;
+package com.emeritus.cms.cms_api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class AssignmentServiceApplication {
+public class CmsApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AssignmentServiceApplication.class, args);
+		SpringApplication.run(CmsApiGatewayApplication.class, args);
 	}
 
 }
